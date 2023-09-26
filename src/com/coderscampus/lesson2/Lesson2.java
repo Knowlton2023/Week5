dca6d9fff47c0851ea59241159520a20b79f3664
Charles Knowlton <clknowlton@icloud.com>
2023-09-26 05:31:20 -0500

    WIP

--- src/com/coderscampus/lesson2/Lesson2.java
+++ src/com/coderscampus/lesson2/Lesson2.java
@@ -1,7 +1,8 @@
 package com.coderscampus.lesson2;
 
 public class Lesson2 {
-
+	public String someVariable = "Hi there";
+	
 	// Access Modifiers = private, public, protected
 	public static void main(String[] args) {
 		
