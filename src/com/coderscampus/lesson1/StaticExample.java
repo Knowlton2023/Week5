f010f4c2b3b8df48e4d10ca37d8222b54f2719c7
Charles Knowlton <clknowlton@icloud.com>
2023-09-26 04:57:21 -0500

    WIP

--- src/com/coderscampus/lesson1/StaticExample.java
+++ src/com/coderscampus/lesson1/StaticExample.java
@@ -3,10 +3,15 @@ package com.coderscampus.lesson1;
 public class StaticExample {
 
 	// static = belongs to the class, not an instance of the class
+	// non-static = belongs to an instance of the class (not the class itself)
+	int someNumber = 10;
 	
 	public static void main(String[] args) {
-
+		StaticExample staticObject = new StaticExample();
 		
+		if (staticObject.someNumber > 11) {
+			System.out.println("say something");
+		}
 		
 	}
 
