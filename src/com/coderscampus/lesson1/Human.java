9e22eb3bac2e4a3ac738d11c0b0b3d1dd16231fc
Charles Knowlton <clknowlton@icloud.com>
2023-09-26 05:01:50 -0500

    WIP

--- src/com/coderscampus/lesson1/Human.java
+++ src/com/coderscampus/lesson1/Human.java
@@ -1,5 +1,19 @@
 package com.coderscampus.lesson1;
 
 public class Human {
+	// instance variable
+	int age;
+	
+	public String getStageOfLife() {
+		if (age < 13) {
+			return "Child";
+		} else if (age <19) {
+			return "Teenager";   
+		} else if (age < 65) {
+			return "Adult";
+		} else {
+			return "Senior";
+		}
 
+	}
 }
