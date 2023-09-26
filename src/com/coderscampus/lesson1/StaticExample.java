4e4729c0973e5758426b07a0296e71fb00518d15
Charles Knowlton <clknowlton@icloud.com>
2023-09-26 04:50:33 -0500

    Initial commit

---
+++ src/com/coderscampus/lesson1/StaticExample.java
@@ -0,0 +1,13 @@
+package com.coderscampus.lesson1;
+
+public class StaticExample {
+
+	// static = belongs to the class, not an instance of the class
+	
+	public static void main(String[] args) {
+
+		
+		
+	}
+
+}
