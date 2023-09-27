81eca16ca093651cd6b4e5268372d061d79bdb2d
Charles Knowlton <clknowlton@icloud.com>
2023-09-27 17:47:15 -0500

    WIP

---
+++ src/com/coderscampus/lesson4/UserService.java
@@ -0,0 +1,11 @@
+package com.coderscampus.lesson4;
+
+import com.coderscampus.lesson3.User;
+
+public class UserService {
+
+	public User createUser (String username, String password) {
+		User user = new User();
+		
+	}
+}
