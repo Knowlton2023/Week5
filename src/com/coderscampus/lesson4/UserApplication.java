1e8a30a3705b123d3e275b9bf6c807e630918e26
Charles Knowlton <clknowlton@icloud.com>
2023-09-30 15:05:45 -0500

    WIP

--- src/com/coderscampus/lesson4/UserApplication.java
+++ src/com/coderscampus/lesson4/UserApplication.java
@@ -7,8 +7,18 @@ public class UserApplication {
 	public static void main(String[] args) {
 		UserService userService = new UserService();
 		
-		User trevorUser = userService.createUser("trevor@craftycodr.com", "password123");
-		System.out.println(trevorUser);
+		// Arrays allow us to store multiple objects (or primitives) into
+		//  ONE variable - Note: an Array is a one type of Data Structure
+		
+		// [0], [1], [2], [3], [4], [5], [6], [7], [8], [9], [10], [11], [12]
+		User[] users = new User[13];
+
+		for (int i = 0; i < 13; i++) {
+			users[i] = userService.createUser("user" + (i+1), "password" + (i+1));
+		}
+
+		System.out.println(users);
+	
 	}
 
 }
