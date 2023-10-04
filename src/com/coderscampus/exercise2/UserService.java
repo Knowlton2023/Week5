f13b61b45dced93f673b966f3d464c5d45982182
Charles Knowlton <clknowlton@icloud.com>
2023-10-04 18:44:46 -0500

    WIP

--- src/com/coderscampus/exercise2/UserService.java
+++ src/com/coderscampus/exercise2/UserService.java
@@ -2,8 +2,6 @@ package com.coderscampus.exercise2;
 
 public class UserService {
 
-	
-	
 	public User createUser (String username, String password, String name) {
 		User user = new User();
 		user.setUsername(username);
