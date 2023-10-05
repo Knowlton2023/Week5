caadfd4ce71d750d0410c7d2edaff6160b3bfda1
Charles Knowlton <clknowlton@icloud.com>
2023-10-04 20:56:57 -0500

    WIP

--- src/com/coderscampus/exercise2/UserApplication.java
+++ src/com/coderscampus/exercise2/UserApplication.java
@@ -4,9 +4,10 @@ public class UserApplication {
 
 	public static void main(String[] args) {
 		UserService userService = new UserService();
+		User user = new User();
 		User[] stringInputs = new User[3];
 
-		stringInputs[0] = "aUsernameHere";
+		stringInputs[0] = user.setUsername("aUsernameHere");
 //		stringInputs[0] = userService.createUser("aUsernameHere", "aPasswordHere", "Somebodies Name");
 //		stringInputs[1] = userService.createUser(null, "aPasswordHere", null);
 //		stringInputs[2] = userService.createUser(null, null, "Somebodies Name");
