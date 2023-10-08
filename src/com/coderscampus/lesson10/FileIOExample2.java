392df9d50300f517463573f74af014da45ca6c3d
Charles Knowlton <clknowlton@icloud.com>
2023-10-08 16:37:06 -0500

    WIP

--- src/com/coderscampus/lesson10/FileIOExample2.java
+++ src/com/coderscampus/lesson10/FileIOExample2.java
@@ -5,7 +5,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
-public class FileIOExample {
+public class FileIOExample2 {
 
 	public static void main(String[] args) {
 
@@ -18,12 +18,6 @@ public class FileIOExample {
 				System.out.println(line);
 			}
 			
-		} catch (FileNotFoundException e) {
-			System.out.println("Oops, the file wasn't found");
-			e.printStackTrace();
-		} catch (IOException e) {
-			System.out.println("Oops, there was an I/O Exception");
-			e.printStackTrace();
 		} finally {
 			try {
 				System.out.println("Closing file reader");
